9185aee082690d4d57214497fc8de43078c95cdc
moises <mbp421421@gmail.com>
2025-09-13 15:25:54 -0500

    Puntos 1,2,3 y 4 terminados en kotlin

--- 1Decalracion.kt
+++ 1Decalracion.kt
@@ -1,17 +1,25 @@
-// 1 declaracion
+// 1Decalracion
 data class Estudiante(val nombre: String, val edad: Int, var promedio: Double)
 
-// 2 Iniciacion
-val e1 = Estudiante("moises", 25, 9.0)
-val e2 = Estudiante("rafael", 22, 8.5)
-val e3 = Estudiante("camila", 21, 7.8)
+fun main() {
+    // 2 Inicialización
+    val e1 = Estudiante("wendy", 30, 9.0)
+    val e2 = Estudiante("Luis", 22, 8.5)
+    val e3 = Estudiante("María", 21, 7.8)
 
-// 3 Recorrido
-val estudiantes = listOf(e1, e2, e3)
+    
+    val estudiantes = listOf(e1, e2, e3)
 
-for (e in estudiantes) {
-    println("${e.nombre} - ${e.edad} - ${e.promedio}")
+    // 3 Recorrido
+    println("Lista de estudiantes:")
+    for (e in estudiantes) {
+        println("${e.nombre} - ${e.edad} - ${e.promedio}")
+    }
+
+    // 4 Modificación
+    e2.promedio = 9.2
+    println("\nDespués de modificar el promedio de ${e2.nombre}:")
+    for (e in estudiantes) {
+        println("${e.nombre} - ${e.edad} - ${e.promedio}")
+    }
 }
-
-// 4 modificacion
-estudiantes[1].promedio = 9.2
